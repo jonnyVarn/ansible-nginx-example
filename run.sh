4fff04d314c874c762a101623f1ae2746a2d4157
jonnyVarn <71185833+jonnyVarn@users.noreply.github.com>
2021-04-03 13:47:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ansible-playbook -i inventory.yml playbooks/nginx.yml
 ansible-playbook -i inventory.yml playbooks/sync.yml
 ansible-playbook -i inventory.yml playbooks/install_docker.yaml
+ansible-playbook -i inventory.yml playbooks/trim.yaml
